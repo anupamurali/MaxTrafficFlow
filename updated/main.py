b7b87f1f3f188fb7d612f0941ea9cd1ec0f6c0eb
alexandermoore <alexmoore700@gmail.com>
2014-12-04 04:40:19 -0500

    ...

--- updated/main.py
+++ updated/main.py
@@ -3,3 +3,10 @@ import road
 import node
 import city_util
 import structure
+import util
+import local_search
+
+newCity = util.basic_city()
+
+hillClimb = local_search.HillClimbing()
+hillClimb.run_algorithm(newCity,objectives.Profit)
--- updated/objectives.py
+++ updated/objectives.py
@@ -1,9 +1,4 @@
-class ObjectiveFunction:
-    def evaluate(self, city):
-        # Returns the objective value of a given city with flows. ALso returns a dictionary of the
-        # individual components of it (ex {"profit": 200, "congestion": 1000}
-        raise Exception("Implement the objective function!")
-
-class ProfitAndCongestion(ObjectiveFunction):
-    def evaluate(self, city):
-        return 0.0, {}
+def profit_and_congestion():
+    # Returns the objective value of a given city with flows. ALso returns a dictionary of the
+    # individual components of it (ex {"profit": 200, "congestion": 1000}
+    return 0.0, {}
