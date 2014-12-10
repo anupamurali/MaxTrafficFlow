6dcc43624628e49b86f898c2e262f696095fc62b
Anupa Murali <anupamurali@college.harvard.edu>
2014-12-10 01:37:54 -0500

    fixed main

--- updated/main.py
+++ updated/main.py
@@ -6,6 +6,7 @@ import structure
 import util
 import local_search
 import objectives
+import time
 
 newCity = util.harder_city_with_cycle()
 city_util.compute_initial_probabilities(newCity)
@@ -14,13 +15,30 @@ city_util.compute_flows(newCity, 150)
 for r in newCity.roads:
     print (r.node1.name, r.node2.name), ":", r.flow
 
-hillClimb = local_search.BruteForce()
-bestCity, bestObj = hillClimb.run_algorithm(newCity,objectives.profit_and_congestion)
-print 'obj  = ',bestObj
+starttime = time.time()
+bruteForce = local_search.BruteForce()
+for _ in xrange(1):
+    bestCity, bestObj = bruteForce.run_algorithm(newCity,objectives.profit_and_congestion)
+bruteForceTime = time.time() - starttime
+starttime = time.time()
+hillClimb = local_search.HillClimbing()
+for _ in xrange(1):
+    bestCityHill, bestObjHill = hillClimb.run_algorithm(newCity,objectives.profit_and_congestion)
+hillClimbTime = time.time() - starttime
+print 'BRUTE FORCE: ',bestObj[0]
+print '    TIME: ', bruteForceTime
 for n in bestCity.nodes:
     print n.name, n.structure['name']
 for r in bestCity.roads:
     print (r.node1.name, r.node2.name), ":", r.flow
+print 'HILL CLIMB: ',bestObjHill[0]
+print '    TIME:', hillClimbTime
+for n in bestCityHill.nodes:
+    print n.name, n.structure['name']
+for r in bestCityHill.roads:
+    print (r.node1.name, r.node2.name), ":", r.flow
+print "OBJECTIVE DIFFERENCE (brute - hill): ",bestObj[0] - bestObjHill[0]
+print "TIME DIFFERENCE (brute - hill): ", bruteForceTime - hillClimbTime
 quit()
 
 
